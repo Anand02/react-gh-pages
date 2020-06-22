eeba440467d801c49c72c6a51a5d00ac3d77c84a
Anand <anand.sg@fleximc.com>
2020-06-22 15:55:50 +0530

    Updates

---
+++ precache-manifest.91a21b45646699332814ac02b73e27cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee43133f8db69e5e2d60678834a628f",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "7b6c3b0e33c1d2af9ec4",
+    "url": "/react-gh-pages/static/css/2.ec274cb8.chunk.css"
+  },
+  {
+    "revision": "47cb85a9168a704428ad",
+    "url": "/react-gh-pages/static/css/main.846eedc1.chunk.css"
+  },
+  {
+    "revision": "7b6c3b0e33c1d2af9ec4",
+    "url": "/react-gh-pages/static/js/2.546972b0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-gh-pages/static/js/2.546972b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47cb85a9168a704428ad",
+    "url": "/react-gh-pages/static/js/main.707be3b3.chunk.js"
+  },
+  {
+    "revision": "1a59da2f382c0241f8c6",
+    "url": "/react-gh-pages/static/js/runtime-main.e9d450ce.js"
+  }
+]);
